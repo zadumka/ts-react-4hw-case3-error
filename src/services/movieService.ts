66f4d851145cd8312c990de1927165e1f23dd3d0
zadumka <alenahas1987@gmail.com>
2025-04-28 14:06:35 +0300

    add hw

---
+++ src/services/movieService.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { Movie } from '../types/movie';
+
+interface TMDBResponse {
+    results: Movie[];
+    total_pages: number;
+}
+
+export const fetchMovies = async (
+    query: string,
+    page: number,
+): Promise<TMDBResponse> => {
+    const response = await axios.get(
+        'https://api.themoviedb.org/3/search/movie',
+        {
+            params: {
+                query,
+                page,
+            },
+            headers: {
+                Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+            },
+        },
+    );
+    return response.data;
+};
